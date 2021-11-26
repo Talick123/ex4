db7e9fc20b8500d9cae40120439b54fc0ed17e1f
nogalevy <nogalevy2@gmail.com>
2021-11-26 16:00:33 +0200

    start to add old code from ex3b and checge to match the named pipe - compiled on ubuntu

--- ex4a1.c
+++ ex4a1.c
@@ -11,22 +11,46 @@
 #include <sys/wait.h>
 #include <sys/time.h>
 
+#include <sys/stat.h> //named pipe
+
+// --------const section------------------------
+
 const int ARR_SIZE = 10000;
 const int ARGC_SIZE = 5;
+const int NUM_OF_GEN = 3;
+
+// --------struct section------------------------
 
 struct Data {
 	pid_t _cpid; //child pid
 	int _prime; //
 };
 
+// --------protoype section------------------------
+
+void fill_array(FILE *input_file, FILE *fifo1_file, FILE *fifo2_file, FILE* fifo3_file, pid_t child[]);
+int count_primes(int arr[]);
+void reset_arr(int arr[], int size_arr);
+void check_argv(int argc );
+bool prime(int num);
+
+// --------main section------------------------
+
 int main(int argc, char *argv[])
 {
   //TODO: chaeck if 1 2 3 processes are connected to fifo
+  pid_t ch_pid[NUM_OF_GEN];
+
   check_argv(argc);
-  mkfifo(argv[1], S_IFIFO | 0644);
-  mkfifo(argv[2], S_IFIFO | 0644);
-  mkfifo(argv[3], S_IFIFO | 0644);
-  mkfifo(argv[4], S_IFIFO | 0644);
+  if(mkfifo(argv[1], S_IFIFO | 0644) == -1 ||
+	  mkfifo(argv[2], S_IFIFO | 0644) == -1 ||
+	  mkfifo(argv[3], S_IFIFO | 0644) == -1 ||
+	  mkfifo(argv[4], S_IFIFO | 0644) == -1)
+  {
+  	puts("mkfifo error");
+  	exit(EXIT_FAILURE);
+  }
+
 
   //TODO: add error if cannot open
   FILE *input_file = fopen(argv[1] ,"r");
@@ -35,11 +59,13 @@ int main(int argc, char *argv[])
   FILE *fifo3_file = fopen(argv[4] ,"w");
   //waits for numbers from all children in order to start(and tell them to start)
 
-  fill_array(input_file, fifo1_file, fifo2_file, fifo3_file);
+  fill_array(input_file, fifo1_file, fifo2_file, fifo3_file, ch_pid);
   //fills array
   //sends back to sender number of times it received that prime
   //sends message to yazranim when the array is filled to sends -1?
   //prints number of different primes received
+
+  //TODO: close fifo
   return EXIT_SUCCESS;
 }
 
@@ -51,45 +77,46 @@ int main(int argc, char *argv[])
 
 //-------------------------------------------------
 // gets pipe of all children and the pid array
-fill_array(FILE *input_file, FILE *fifo1_file, FILE *fifo2_file, FILE* fifo3_file, pid_t child[]);
+void fill_array(FILE *input_file, FILE *fifo1_file, FILE *fifo2_file, FILE* fifo3_file, pid_t child[])
 {
 	int primes_count[ARR_SIZE]; //count in each index the number of times father receive this number
-	int filled = 0, index;
+	int filled = 0;
 	struct Data data;
 	reset_arr(primes_count, ARR_SIZE);
-  int status;
 
-	while(filled < ARR_SIZE)
+
+	while(filled < ARR_SIZE) // while EOF ?
 	{
     //read process id and num
-		status =fscanf(fdr, "%d %d", &data._cpid, &data._prime)
+		fscanf(input_file, "%d %d", &data._cpid, &data._prime);
 
 		//check which child sent the number depend on the pid
 		//send the counter of the prime num
 		if (data._cpid == child[0])
-    {
-      fprintf(fifo1_file, " &d", &primes_count[data._prime]);
-      fflush(fifo1_file)
-    }
+		{
+		  fprintf(fifo1_file, " %d", primes_count[data._prime]);
+		  fflush(fifo1_file);
+		}
 		else if (data._cpid == child[1])
-    {
-      fprintf(fifo2_file, " &d", &primes_count[data._prime]);
-      fflush(fifo2_file)
-    }
+		{
+		  fprintf(fifo2_file, " %d", primes_count[data._prime]);
+		  fflush(fifo2_file);
+		}
 		else if (data._cpid == child[2])
-    {
-      fprintf(fifo3_file, " &d", &primes_count[data._prime]);
-      fflush(fifo3_file)
-    }
-
+		{
+		  fprintf(fifo3_file, " %d", primes_count[data._prime]);
+		  fflush(fifo3_file);
+		}
 		primes_count[data._prime]++;	//adds to counter
 		filled++;					//increases fill number
 	}
-	//kills children
-    fprintf(fifo1_file, " &d", -1);
-    fprintf(fifo1_file, " &d", -1);
-    fprintf(fifo1_file, " &d", -1);
 
+	//kills children
+    fprintf(fifo1_file, " %d", -1);
+    fprintf(fifo2_file, " %d", -1);
+    fprintf(fifo3_file, " %d", -1);
+
+	//add min max primes
 	printf("The number of different integers received is: %d\n",
 				 count_primes(primes_count));
 }
@@ -129,27 +156,4 @@ void check_argv(int argc )
 		printf("Error! Incorrect number of arguments.\n");
 		exit(EXIT_FAILURE);
 	}
-}
-
-//-------------------------------------------------
-//gets integer and check if is prime
-bool prime(int num)
-{
-	int i;
-	for(i = 2; i*i < num; i++)
-	{
-		if(num % i == 0)
-			return false;
-	}
-	return true;
-}
-
-//-------------------------------------------------
-//catch signal of SIGTERM
-void catch_sigterm(int signum)
-{
-	signal(SIGTERM, catch_sigterm);
-
-	printf("Child process %d sent %d new primes\n", getpid(), counter);
-	exit(EXIT_SUCCESS);
 }
--- ex4a2.c
+++ ex4a2.c
@@ -3,6 +3,7 @@
 
 // --------include section------------------------
 
+#include <string.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,15 +12,80 @@
 #include <sys/wait.h>
 #include <sys/time.h>
 
+//named pipe
+#include <sys/stat.h>
 
-int main()
+const int ARR_SIZE = 1000;
+
+struct Data {
+	pid_t _cpid; //child pid
+	int _prime; //
+};
+
+void handle_child(FILE *fifo_w, FILE *fifo_r);
+bool prime(int num);
+
+int main(int argc, char *argv[])
 {
-  //receives via argument vector the number to send to aba
-  //sends to aba starting number
-  //waits for number back in order to start
-  //randomizes numbers in an infinite loop
-  //each time there is prime, sends to first program through named pipe
-  //when ending message received, prints how many new primes sent
-  //ends
-  return EXIT_SUCCESS;
+	//TODO: add error if cannot open
+	//check 3 argc
+	//s
+	FILE *input_file = fopen(argv[1] ,"r");
+	char *fifo_name = strcat("fifo", argv[2]);
+	FILE *fifo_file = fopen(fifo_name ,"w");
+
+	handle_child(input_file, fifo_file);
+	//receives via argument vector the number to send to aba
+	//sends to aba starting number
+	//waits for number back in order to start
+	//randomizes numbers in an infinite loop
+	//each time there is prime, sends to first program through named pipe
+	//when ending message received, prints how many new primes sent
+	//ends
+	return EXIT_SUCCESS;
+}
+
+
+
+
+//all children write through the same pipe
+//children read through 3 seperate pipes?
+//gets pipe_fd1[] = pipe to write parent
+//gets pipe_r[] = pipe to read from parent
+void handle_child(FILE *fifo_w, FILE *fifo_r)
+{
+	struct Data data;
+	data._cpid = getpid();
+	int num;
+
+
+	while(true)
+	{
+		num = rand()%(ARR_SIZE -1) + 2; //randomize num between 2 to 1000
+
+		if(prime(num))
+		{
+			//send to father num + getpid() using pipe_fd1
+			data._prime = num; // save prime num in struct
+			// write to pipe the data (pid and prime num)
+			fprintf(fifo_w, " %d %d", data._cpid, data._prime );
+			fscanf(fifo_r," %d", &num);
+		}
+		/*if (num == NEW_PRIME)
+			counter++;
+			*/
+	}
+}
+
+//-------------------------------------------------
+//gets integer and check if is prime
+bool prime(int num)
+{
+	int i;
+	for(i = 2; i*i < num; i++)
+	{
+		if(num % i == 0)
+			return false;
+	}
+	return true;
 }
