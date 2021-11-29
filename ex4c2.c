549c2848ca6e893cf4a15c7f546ea9cb05cd95c4
Tali Kalev <talkalev@gmail.com>
2021-11-29 11:40:11 +0200

    program c compiles

--- ex4c2.c
+++ ex4c2.c
@@ -22,6 +22,12 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
 
 // --------const and enum section------------------
 
@@ -29,6 +35,7 @@ enum Requests {PRIME, PALINDROME};
 enum Answers {FALSE = 0, TRUE = 1, FAIL = -1}; //MAYBE JUST CONVERT BOOLS?
 enum Exists_answers {DOESNTEXIST, DOESEXIST};
 const int REGISTER = 1;
+int msqid2;
 
 // --------struct section------------------------
 
@@ -57,7 +64,7 @@ struct Msgbuf2{ //to customer
 // --------prototype section---------------------
 
 void catch_int(int signum);
-void read_requests(int misqid1, struct Data1 &msg1, int misqid2, struct Data2 &msg2);
+void read_requests(int msqid1, struct Msgbuf1 *msg1, int msqid2, struct Msgbuf2 *msg2);
 int is_prime(int num);
 int is_palindrome(char *string);
 
@@ -67,11 +74,11 @@ int main()
 {
   struct Msgbuf1 msg1;
   struct Msgbuf2 msg2;
-  int msqid1, msqid2;
+  int msqid1;
   key_t key1, key2;
 
   signal(SIGINT, catch_int);
-	
+
   //creating external id for message queue
   if((key2 = ftok(".",'d')) == -1 ||
       (key1 = ftok(".", 'c')) == -1)
@@ -81,14 +88,16 @@ int main()
   }
 
   //creating internal id for message queue
-  if((msqid2 = msgget(key2, 0600 | IPC_CREAT | IPC_EXCL)) == -1 ||
+  if(((msqid2 = msgget(key2, 0600 | IPC_CREAT | IPC_EXCL)) == -1 && errno != EEXIST)||
       ((msqid1 = msgget(key1, 0)) == -1))
   {
     perror("msgget failed\n");
     exit(EXIT_FAILURE);
   }
+  printf("msqid1 is: %d\n",msqid1);
+  printf("msqid2 is: %d\n",msqid2);
 
-  read_requests(msqid1, msg1, msqid2, msg2);
+  read_requests(msqid1, &msg1, msqid2, &msg2);
 
   return EXIT_SUCCESS;
 }
@@ -97,10 +106,8 @@ int main()
 
 void catch_int(int signum)
 {
-	// Q: misqid2 global?
-	//release queue - can it do it here or does it need to do in main
-	//ends
-	if(msgctl(misqid2, IPC_RMID, NULL) == -1)
+
+	if(msgctl(msqid2, IPC_RMID, NULL) == -1)
 	{
 		perror("msgctl faild\n");
 		exit(EXIT_FAILURE);
@@ -110,53 +117,53 @@ void catch_int(int signum)
 
 //-------------------------------------------------
 
-void read_requests(int misqid1, struct Data1 &msg1, int misqid2, struct Data2 &msg2)
+void read_requests(int msqid1, struct Msgbuf1 *msg1, int msqid2, struct Msgbuf2 *msg2)
 {
   int status;
 
   while(true)
   {
-    if(msgrcv(msqid2, &msg2, sizeof(struct Data2), 2, 0) == -1)
+    if(msgrcv(msqid2, msg2, sizeof(struct Data2), 2, 0) == -1)
     {
       perror("msgrcv failed\n");
       exit(EXIT_SUCCESS);
     }
 
     //preparing to ask registry server to check if customer exists
-    msg1._type = REGISTER;
-    msg1._data1._cpid = msg2._data2._cpid;
-    msg1._data1._status = DOESEXIST;
-    if(msgsnd(msqid1, &msg1, sizeof(struct Data1), 0) == -1) //sending request
+    (*msg1)._type = REGISTER;
+    (*msg1)._data1._cpid = (*msg2)._data2._cpid;
+    (*msg1)._data1._status = DOESEXIST;
+    if(msgsnd(msqid1, msg1, sizeof(struct Data1), 0) == -1) //sending request
     {
       perror("msgsnd failed\n");
       exit(EXIT_FAILURE);
     }
-    if(msgrcv(msqid1, &msg1, sizeof(struct Data2), getpid(), 0) == -1)//receiving answer (maybe sleep a bit before)
+    if(msgrcv(msqid1, msg1, sizeof(struct Data1), getpid(), 0) == -1)//receiving answer (maybe sleep a bit before)
     {
       perror("msgrcv failed\n");
       exit(EXIT_SUCCESS);
     }
 
-    if(msg1._data1._status == DOESNTEXIST) //checking if customer is in registry server
+    if((*msg1)._data1._status == DOESNTEXIST) //checking if customer is in registry server
       status = FAIL;
 
     else //If customer does exist in registry
     {
-      switch(msg2._data2._status) //checking request
+      switch((*msg2)._data2._status) //checking request
       {
         case PRIME:
-          status = is_prime(msg2._data._num);
+          status = is_prime((*msg2)._data2._num);
           break;
         case PALINDROME:
-          status = is_palindrome(msg2._data._string);
+          status = is_palindrome((*msg2)._data2._string);
           break;
       }
     }
 
     //preparing to send back to customer the answer
-    msg2._type = msg2._data2._cpid;
-    msg2._data2._status = status;
-    if(msgsnd(msqid2, &msg2, sizeof(struct Data), 0) == -1)
+    (*msg2)._type = (*msg2)._data2._cpid;
+    (*msg2)._data2._status = status;
+    if(msgsnd(msqid2, msg2, sizeof(struct Data2), 0) == -1)
     {
       perror("msgsnd failed\n");
       exit(EXIT_FAILURE);
@@ -182,26 +189,24 @@ int is_prime(int num)
 int is_palindrome(char *string)
 {
 	int length;
-   char *forward, *reverse;
-   length = strlen(string);
-   forward = string;
-   reverse = forward + length - 1;
-   for (forward = string; reverse >= forward;)
-	 {
-      if (*reverse == *forward)
-			{
-         reverse--;
-         forward++;
-      }
-			 else
-         break;
-   }
-	 if (forward > reverse)
-      return TRUE;
-   else
-      return FALSE;
+	char *forward, *reverse;
+	length = strlen(string);
+	forward = string;
+	reverse = forward + length - 1;
+	for (forward = string; reverse >= forward;)
+	{
+		if (*reverse == *forward)
+		{
+			reverse--;
+			forward++;
+		}
+		else
+		 break;
+	}
+	if (forward > reverse)
+		return TRUE;
+	else
+		return FALSE;
 }
-
-
 
 //-------------------------------------------------
--- ex4c3.c
+++ ex4c3.c
@@ -7,11 +7,18 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
 
 // --------const and enum section-------------------
 
 enum Requests_1 {ADD, INARR, REMOVE}; // add and remvoe
 enum Add_answers {ADDED, EXISTS, FULL}; // need just full
+enum App_answers {FALSE = 0, TRUE = 1, FAIL = -1};
 enum Requests_2 {PRIME, PALINDROME}; // need
 const int REGISTER = 1; //can change this to type enum that equals 1 and 2
 const int APPLICATION = 2;
@@ -42,70 +49,69 @@ struct Msgbuf2{ //msg to application server
 
 // --------prototype section---------------------
 
-void registr(int misqid1, struct Data1 &msg1);
-void read_from_user(int misqid1, struct Data1 &msg1, int misqid2, struct Data2 &msg2);
-void is_num_prime(int misqid2, struct Data1 &msg2, int num);
-void is_str_palindrome(int misqid2, struct Data1 &msg2, char *str);
-void exit_from_system(int misqid1, struct Data1 &msg1);
+void registr(int misqid1, struct Msgbuf1 *msg1);
+void read_from_user(int misqid1, struct Msgbuf1 *msg1, int misqid2, struct Msgbuf2 *msg2);
+void is_num_prime(int misqid2, struct Msgbuf2 *msg2, int num);
+void is_str_palindrome(int misqid2, struct Msgbuf2 *msg2, char *str);
+void exit_from_system(int misqid1, struct Msgbuf1 *msg1);
 void perror_and_exit(char *action);
 
 // --------main section------------------------
 
 int main()
 {
-  struct Msgbuf1 msg1;
-  struct Msgbuf2 msg2;
-  int msqid1, msqid2;
-  key_t key1, key2;
-
-  signal(SIGINT, catch_int);
+	struct Msgbuf1 msg1;
+	struct Msgbuf2 msg2;
+	int msqid1, msqid2;
+	key_t key1, key2;
 
 	//T: should we put the next 3 things in function??
 	msg2._data2._cpid = msg1._data1._cpid =  getpid();
 
-  //creating external id for message queue
-  if((key2 = ftok(".",'d')) == -1 ||
-			(key1 = ftok(".", 'c')) == -1)
-			error_and_exit("ftok");
+	//creating external id for message queue
+	if((key2 = ftok(".",'d')) == -1 ||
+		(key1 = ftok(".", 'c')) == -1)
+		perror_and_exit("ftok failed");
 
-  //creating internal id for message queue
-  if((msqid2 = msgget(key2, 0)) == -1 ||
-      ((msqid1 = msgget(key1, 0)) == -1))
-			error_and_exit("msgget");
+	//creating internal id for message queue
+	if((msqid2 = msgget(key2, 0)) == -1 ||
+		((msqid1 = msgget(key1, 0)) == -1))
+		perror_and_exit("msgget failed");
+	printf("msqid1 is: %d\n",msqid1);
+	printf("msqid2 is: %d\n",msqid2);
 
-  registr(msqid1, msg1);
-  read_from_user(msqid1, msg1, msqid2, msg2);
+	registr(msqid1, &msg1);
+	read_from_user(msqid1, &msg1, msqid2, &msg2);
 
-  return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
 
 //-------------------------------------------------
 
-void registr(int misqid1, struct Data1 &msg1)
+void registr(int misqid1, struct Msgbuf1 *msg1)
 {
-	msg1._data1._status = ADD;
-	msg1._type = REGISTER;
+	(*msg1)._data1._status = ADD;
+	(*msg1)._type = REGISTER;
 
 	//send request to register
-	if(msgsnd(misqid1, &msg1, sizeof(struct Data), 0) == -1)
-		perror_and_exit("msgsnd");
+	if(msgsnd(misqid1, msg1, sizeof(struct Data1), 0) == -1)
+		perror_and_exit("msgsnd failed");
 
 	//read result -- can add to this if the check if full?
-	if(msgrcv(misqid1, &msg1, sizeof(struct Data), getpid(), 0) == -1)
-		perror_and_exit("msgrcv");
+	if(msgrcv(misqid1, msg1, sizeof(struct Data1), getpid(), 0) == -1)
+		perror_and_exit("msgrcv failed");
 
 	//check if succeeded
-	if(msg1._data1._status == FULL)
-		perror_and_exit("registration");
+	if((*msg1)._data1._status == FULL)
+		perror_and_exit("registration failed");
 
 	//...else register success . so ok
 }
 
 //-------------------------------------------------
 
-void read_from_user(int misqid1, struct Data1 &msg1, int misqid2, struct Data2 &msg2)
+void read_from_user(int misqid1, struct Msgbuf1 *msg1, int misqid2, struct Msgbuf2 *msg2)
 {
-	bool end = false;
 	char req;
 	char *str = NULL;
 	int num;
@@ -121,7 +127,7 @@ void read_from_user(int misqid1, struct Data1 &msg1, int misqid2, struct Data2 &
 				break;
 			case 's':
 				scanf(" %s", str);
-				is_palindrome(misqid2, msg2, str);
+				is_str_palindrome(misqid2, msg2, str);
 				break;
 			case 'e':
 				exit_from_system(misqid1, msg1);
@@ -134,51 +140,53 @@ void read_from_user(int misqid1, struct Data1 &msg1, int misqid2, struct Data2 &
 
 //-------------------------------------------------
 
-void is_num_prime(int misqid2, struct Data1 &msg2, int num)
+void is_num_prime(int misqid2, struct Msgbuf2 *msg2, int num)
 {
-	msg2._type = APPLICATION;
-	msg2._data2._status = PRIME;
-	msg2._data2._num = num;
+	(*msg2)._type = APPLICATION;
+	(*msg2)._data2._status = PRIME;
+	(*msg2)._data2._num = num;
 
-	if(msgsnd(misqid2, &msg2, sizeof(struct Data), 0) == -1)
-		perror_and_exit("msgsnd");
+	if(msgsnd(misqid2, msg2, sizeof(struct Data2), 0) == -1)
+		perror_and_exit("msgsnd failed");
 
-	if(msgrcv(misqid2, &msg2, sizeof(struct Data), getpid(), 0) == -1)
-		perror_and_exit("msgrcv");
+	if(msgrcv(misqid2, msg2, sizeof(struct Data2), getpid(), 0) == -1)
+		perror_and_exit("msgrcv failed");
 
 	//we can do it in one printf
-	switch (msg2._data2._status)
+	switch ((*msg2)._data2._status)
 	{
 		case FALSE:
-			printf("%d is not a prime", num);
+			printf("%d is not a prime\n", num);
 			break;
 		case TRUE:
-			printf("%d is a prime", num);
+			printf("%d is a prime\n", num);
 			break;
+		case FAIL:
+			printf("I'm not registered\n");
 	}
 }
 
 //-------------------------------------------------
 
-void is_str_palindrome(int misqid2, struct Data1 &msg2, char *str)
+void is_str_palindrome(int misqid2, struct Msgbuf2 *msg2, char *str)
 {
-	msg2._type = APPLICATION;
-	msg2._data2._status = PALINDROME;
-	msg2._data2._string = str;
+	(*msg2)._type = APPLICATION;
+	(*msg2)._data2._status = PALINDROME;
+	(*msg2)._data2._string = str;
 
-	if(msgsnd(misqid2, &msg2, sizeof(struct Data), 0) == -1)
-		perror_and_exit("msgsnd");
+	if(msgsnd(misqid2, msg2, sizeof(struct Data2), 0) == -1)
+		perror_and_exit("msgsnd failed");
 
-	if(msgrcv(misqid2, &msg2, sizeof(struct Data), getpid(), 0) == -1)
-		perror_and_exit("msgrcv");
+	if(msgrcv(misqid2, msg2, sizeof(struct Data2), getpid(), 0) == -1)
+		perror_and_exit("msgrcv failed");
 
 	//we can do it in one printf
-	switch (msg2._data2._status) {
+	switch ((*msg2)._data2._status) {
 		case FALSE:
-			printf("%s is not a palindrome", str);
+			printf("%s is not a palindrome\n", str);
 			break;
 		case TRUE:
-			printf("%s is a palindrome", str);
+			printf("%s is a palindrome\n", str);
 			break;
 	}
 }
@@ -186,13 +194,13 @@ void is_str_palindrome(int misqid2, struct Data1 &msg2, char *str)
 //-------------------------------------------------
 
 // ?
-void exit_from_system(int misqid1, struct Data1 &msg1)
+void exit_from_system(int misqid1, struct Msgbuf1 *msg1)
 {
-	msg1._type = REGISTER;
-	msg1._data1._status = REMOVE;
+	(*msg1)._type = REGISTER;
+	(*msg1)._data1._status = REMOVE;
 
-	if(msgsnd(misqid1, &msg1, sizeof(struct Data), 0) == -1)
-		perror_and_exit("msgsnd");
+	if(msgsnd(misqid1, msg1, sizeof(struct Data1), 0) == -1)
+		perror_and_exit("msgsnd failed");
 
 	printf("bye\n");
 	exit(EXIT_SUCCESS);
@@ -202,6 +210,6 @@ void exit_from_system(int misqid1, struct Data1 &msg1)
 
 void perror_and_exit(char *action)
 {
-	perror("%s failed.\n", action);
+	perror(action);
 	exit(EXIT_FAILURE);
 }
